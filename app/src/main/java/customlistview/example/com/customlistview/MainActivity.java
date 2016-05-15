8990588e4f1241c6581d2bdecee623ed43b10139
Larry <s890506@gmail.com>
2016-05-15 22:55:13 +0800

    add: click event.

--- app/src/main/java/customlistview/example/com/customlistview/MainActivity.java
+++ app/src/main/java/customlistview/example/com/customlistview/MainActivity.java
@@ -1,23 +1,26 @@
 package customlistview.example.com.customlistview;
-import android.os.Bundle;
+
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private ListView list;
-    int[] logos = new int[] { R.drawable.img001, R.drawable.img002,
-            R.drawable.img003 };
-    String[] names = new String[] { "超級球", "高級球", "珍貴球" };
-    String[] lists = new String[] { "Great Ball", "Ultra Ball", "Cherish Ball" };
+    private ListView listView;
+    int[] logos = new int[]{R.drawable.img001, R.drawable.img002,
+            R.drawable.img003};
+    String[] names = new String[]{"超級球", "高級球", "珍貴球"};
+    String[] lists = new String[]{"Great Ball", "Ultra Ball", "Cherish Ball"};
 
     MyAdapter adapter = null;
 
@@ -25,11 +28,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = (ListView) findViewById(R.id.list);
+        listView = (ListView) findViewById(R.id.list);
 
         adapter = new MyAdapter(this);
 
-        list.setAdapter(adapter);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int arg2,
+                                    long arg3) {
+                // TODO Auto-generated method stub
+                ListView listView = (ListView) arg0;
+                Toast.makeText(
+                        MainActivity.this,
+                        "ID：" + arg3 +
+                                "   球名：" + listView.getItemAtPosition(arg2).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public class MyAdapter extends BaseAdapter {
